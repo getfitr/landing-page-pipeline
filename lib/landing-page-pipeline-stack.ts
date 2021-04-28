b97e93a604de0621a81785bcc341b646997be805
Carsten Koch <carsten.b.koch@gmail.com>
2021-04-28 09:41:07 +0200

    feat: finished test pipeline

--- lib/landing-page-pipeline-stack.ts
+++ lib/landing-page-pipeline-stack.ts
@@ -1,9 +1,125 @@
-import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as lambda from '@aws-cdk/aws-lambda';
+import { App, Stack, StackProps, SecretValue } from '@aws-cdk/core';
 
-export class LandingPagePipelineStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-  }
+export interface LandingPagePipelineStackProps extends StackProps {
+  readonly lambdaCode: lambda.CfnParametersCode;
+  readonly githubToken: string;
+  // readonly repoName: string
 }
+
+export class LandingPagePipelineStack extends Stack {
+  constructor(app: App, id: string, props: LandingPagePipelineStackProps) {
+    super(app, id, props);
+
+    const cdkBuild = new codebuild.PipelineProject(this, 'LandingPageCdkBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: 'npm install',
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'npm run cdk synth -- -o dist'
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'dist',
+          files: [
+            'LambdaStack.template.json',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    const lambdaBuild = new codebuild.PipelineProject(this, 'LandingPageLambdaBuild', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+              'cd lambda',
+              'npm install',
+            ],
+          },
+          build: {
+            commands: 'npm run build',
+          },
+        },
+        artifacts: {
+          'base-directory': 'lambda',
+          files: [
+            'index.js',
+            'node_modules/**/*',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    const sourceOutput = new codepipeline.Artifact('LandingPageSrcOutput');
+    const cdkBuildOutput = new codepipeline.Artifact('LandingPageCdkBuildOutput');
+    const lambdaBuildOutput = new codepipeline.Artifact('LandingPageLambdaBuildOutput');
+
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      restartExecutionOnUpdate: true,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+              actionName: 'Checkout',
+              output: sourceOutput,
+              owner: 'getfitr',
+              repo: 'landing-page-pipeline',
+              oauthToken: SecretValue.plainText(props.githubToken),
+              trigger: codepipeline_actions.GitHubTrigger.WEBHOOK,
+            }),
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'Lambda_Build',
+              project: lambdaBuild,
+              input: sourceOutput,
+              outputs: [lambdaBuildOutput],
+            }),
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'CDK_Build',
+              project: cdkBuild,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+        {
+          stageName: 'Deploy',
+          actions: [
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: 'Lambda_CFN_Deploy',
+              templatePath: cdkBuildOutput.atPath('LambdaStack.template.json'),
+              stackName: 'LambdaDeploymentStack',
+              adminPermissions: true,
+              parameterOverrides: {
+                ...props.lambdaCode.assign(lambdaBuildOutput.s3Location),
+              },
+              extraInputs: [lambdaBuildOutput],
+            }),
+          ],
+        },
+      ],
+    });
+  }
+};
